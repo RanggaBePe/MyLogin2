fd1ea6def8c8d9f3aeda50f3f7b3f5a3b90d737a
Meliaochy <meliaiko5588@gmail.com>
2018-08-26 00:59:27 +0700

    melia commit

---
+++ app/src/main/java/com/example/android/mylogin/util/Server.java
@@ -0,0 +1,9 @@
+package com.example.android.mylogin.util;
+
+/**
+ * Created by MELIA IKO.
+ */
+
+public class Server {
+    public static final String URL = "http://sasmitoh.nitarahmawati.my.id/";
+}
